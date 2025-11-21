9f0823e40f3bf36ea8814c36a3e0d2b2441c8541
finanovita <finanovita479@gmail.com>
2025-11-21 15:32:41 +0700

    Menambahkan state txtNama, txtAlamat, txtGender agar form dapat menyimpan input pengguna.

---
+++ app/src/main/java/com/example/navigasidata/view/Formulir.kt
@@ -0,0 +1,35 @@
+package com.example.navigasidata.view
+
+import com.example.navigasidata.R
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.DividerDefaults.Thickness
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    pilihanJK: List<String>,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
+){
+    var txtNama by rememberSaveable { mutableStateOf("")}
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("")}
+
+
+}
